c15f282bcc4ef55712ab92d58fc1eda35d093eb1
Jonas Haller <jonas.haller.jh@gmail.com>
2019-12-09 20:41:33 +0100

    Added TODOs for Iterator

---
+++ Übung 7 Design Patterns/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Übung_7_Design_Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArr = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+            var stringEnumerator = stringArr.GetEnumerator();
+            
+            for (int i = 0; i < stringArr.Length; i++)
+            {
+                stringEnumerator.MoveNext();
+                Console.WriteLine(stringEnumerator.Current);
+            }
+
+            foreach (var s in stringArr)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ Übung 7 Design Patterns/Readme.md
@@ -0,0 +1,5 @@
+Welche Methoden/Properties müssen Implementierer des IEnumerator Interface besitzen?
+* Current => Das momentane Element
+* Dispose() => Freigabe, Zurückgabe, oder Zurücksetzen von nicht verwalteten Ressourcen
+* MoveNext() => Geht zum nächsten Element
+* Reset() => Setzt den Enumerator auf seine anfängliche Position vor dem ersten Element zurück.
