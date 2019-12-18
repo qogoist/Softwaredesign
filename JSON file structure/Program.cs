19bb8de6c3e8129d4ec9d400bca70b033dfc2657
Jonas Haller <jonas.haller.jh@gmail.com>
2019-12-18 10:03:48 +0100

    added json files

---
+++ JSON file structure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSON_file_structure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
